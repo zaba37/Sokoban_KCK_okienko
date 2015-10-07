29a5eced93235ce8f499a96ab0345401bba46354
zaba <zaba37@gmail.com>
2015-10-07 21:32:16 +0200

    added ranking screen, logos, and new buttons

--- Sokoban/Sokoban/Form1.cs
+++ Sokoban/Sokoban/Form1.cs
@@ -13,9 +13,12 @@ namespace Sokoban
 {
     public partial class Menu : Form
     {
-        CustomButton cbNewGame;
-        CustomButton cbRanking;
-        CustomButton cbExit;
+        Ranking ranking;
+        private CustomButton cbNewGame;
+        private CustomButton cbRanking;
+        private CustomButton cbExit;
+        private Bitmap pngLogo;
+        private PictureBox logo;
 
         public Menu()
         {
@@ -23,15 +26,23 @@ namespace Sokoban
             FormBorderStyle = FormBorderStyle.None;
             WindowState = FormWindowState.Maximized;
 
-            InitializeComponent();
+            logo = new PictureBox();
+            pngLogo = new Bitmap(@"Drawable\logoMenu.png");
+            logo.BackColor = Color.Transparent;
+            logo.Image = pngLogo;
+            logo.Width = pngLogo.Width;
+            logo.Height = pngLogo.Height;
+            logo.Location = new Point(250,20);
 
-            cbNewGame = new CustomButton(@"Buttons\MenuButtons\NewGameNormal.png", @"Buttons\MenuButtons\NewGamePress.png", @"Buttons\MenuButtons\NewGameFocus.png", 20, 20, "NewGameTag");
-            cbRanking = new CustomButton(@"Buttons\MenuButtons\RankingNormal.png", @"Buttons\MenuButtons\RankingPress.png", @"Buttons\MenuButtons\RankingFocus.png", 20, 150, "ResumeTag");
-            cbExit = new CustomButton(@"Buttons\MenuButtons\ExitNormal.png", @"Buttons\MenuButtons\ExitPress.png", @"Buttons\MenuButtons\ExitFocus.png", 20, 300, "ExitTag");
-            
+            cbNewGame = new CustomButton(@"Buttons\MenuButtons\NewGameNormal.png", @"Buttons\MenuButtons\NewGamePress.png", @"Buttons\MenuButtons\NewGameFocus.png", 450, 350, "NewGameTag");
+            cbRanking = new CustomButton(@"Buttons\MenuButtons\RankingNormal.png", @"Buttons\MenuButtons\RankingPress.png", @"Buttons\MenuButtons\RankingFocus.png", 480, 450, "RankingTag");
+            cbExit = new CustomButton(@"Buttons\MenuButtons\ExitNormal.png", @"Buttons\MenuButtons\ExitPress.png", @"Buttons\MenuButtons\ExitFocus.png", 540, 550, "ExitTag");
+
+            this.Controls.Add(logo);
             this.Controls.Add(cbNewGame);
             this.Controls.Add(cbRanking);
             this.Controls.Add(cbExit);
+            this.BackgroundImage = new Bitmap(@"Drawable\Wall_Beige.png");
 
             cbNewGame.MouseClick += new MouseEventHandler(mouseClick);
             cbRanking.MouseClick += new MouseEventHandler(mouseClick);
@@ -43,11 +54,20 @@ namespace Sokoban
             if (e.Button == MouseButtons.Left)
             {
                 switch(((CustomButton)sender).Tag.ToString()){
+                    case "NewGameTag":
+                        break;
+                    case "RankingTag":
+                        if(ranking == null){
+                            ranking = new Ranking();
+                            ranking.Tag = this;
+                        }
+                        ranking.Show(this);
+                        this.Hide();
+                        break;
                     case "ExitTag":
                         Application.Exit();
                         break;
                 }
-
             }
         }
     }
---
+++ Sokoban/Sokoban/Ranking.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sokoban
+{
+    public partial class Ranking : Form
+    {
+        private CustomButton cbBack;
+        private CustomButton cbArrowUp;
+        private CustomButton cbArrowDown;
+        private Bitmap pngLogo;
+        private PictureBox logo;
+
+        public Ranking()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+
+            pngLogo = new Bitmap(@"Drawable\logoRanking.png");
+            logo = new PictureBox();
+            logo.BackColor = Color.Transparent;
+            logo.Image = pngLogo;
+            logo.Width = pngLogo.Width;
+            logo.Height = pngLogo.Height;
+            logo.Location = new Point(250,20);
+
+            cbBack = new CustomButton(@"Buttons\RankingButtons\BackNormal.png", @"Buttons\RankingButtons\BackPress.png", @"Buttons\RankingButtons\BackFocus.png", 20, 350, "BackTag");
+
+            this.BackgroundImage = new Bitmap(@"Drawable\Wall_Beige.png");
+
+            this.Controls.Add(logo);
+            this.Controls.Add(cbBack);
+
+            cbBack.MouseClick += new MouseEventHandler(mouseClick);
+        }
+
+        private void mouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                switch (((CustomButton)sender).Tag.ToString())
+                {
+                    case "ArrowUpTag":
+                        break;
+                    case "ArrowDownTag":
+                        break;
+                    case "BackTag":
+                        var menu = (Menu)Tag;
+                        menu.Show();
+                        this.Hide();
+                        break;
+                }
+            }
+        }
+    }
+}
